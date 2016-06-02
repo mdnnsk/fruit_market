2bf66174b2f163a5ca785e9d23f1f0d2d97ed3bf
ngorbunov <nick_g@outlook.com>
2016-06-01 21:55:15 -0500

    add minor styling and logic

--- script_2.js
+++ script_2.js
@@ -9,24 +9,33 @@ var minNum = 0.01;
 var maxNum = 0.50;
 var randomPrice = 0;
 
+var endPrice = 0;
 
 var getPrice = function(){
 
- changePrice = Math.floor(Math.random() * 10);
- startPrice = changePrice;
-
  var randomChange = Math.floor(Math.random() * 2);
- randomPrice = (Math.random() * (maxNum - minNum) + minNum);
- // var randomTens = Math.floor(Math.random() * 5.00);
+ var randomPrice = ((Math.random() * 0.50) + 0.005);
+console.log(randomPrice + " randomNumber");
+ var changePrice = Math.floor(Math.random() * 10);
+ var adjustPrice = 0;
 
- if (randomChange === 0){
-   endPrice = (changePrice - randomPrice).toFixed(2);
-   //endPrice = (((startPrice - randomPrice) - randomTens) + 1.00).toFixed(2);
- }
- else {
-   endPrice = (changePrice + randomPrice).toFixed(2);
-   //endPrice = (((startPrice + randomPrice) + randomTens) - 0.51).toFixed(2);
- }
+// Looks to see if endPrice has been updated. Returns only on first time pass then falls through after each pass
+ if (endPrice === 0)
+   { if (randomChange === 0)
+     { endPrice = Number((changePrice - randomPrice).toFixed(2)); }
 
- document.getElementById('randomDisplay').innerHTML = endPrice;
-};
+       else { endPrice = Number((changePrice + randomPrice).toFixed(2)); }
+       document.getElementById('randomDisplay').innerHTML = endPrice; }
+
+// looks to adjust endPrice either plus or negative and makes sure endPrice stays with +/-0.50 ofr previous endPrice
+ else
+ { if (randomChange === 0)
+   { endPrice = (endPrice - randomPrice).toFixed(2);
+     endPrice = Number(endPrice - Math.floor(randomPrice)); }
+
+       else { endPrice = (endPrice + randomPrice).toFixed(2);
+       endPrice = Number(endPrice + Math.floor(randomPrice)); }
+
+       document.getElementById('randomDisplay').innerHTML = endPrice; }
+console.log("endPrice = " + endPrice);
+ };
